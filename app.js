2c0e5c05aae816633869b6a382176ee29fdc813d
Collin Sheehan <collinsheehan15@gmail.com>
2022-03-13 19:32:53 -0400

    second upload

---
+++ app.js
@@ -0,0 +1,37 @@
+//NAVBAR
+
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+    links.classList.toggle("show-links");
+});
+
+//CAROUSEL
+
+let imgIndex = 1;
+showImg(imgIndex);
+
+function nextImg(n) {
+    showImg(imgIndex += n);
+}
+
+function currentImage(n) {
+    showImg(imgIndex = n);
+}
+
+function showImg(n) {
+    let i;
+    let images = document.getElementsByClassName("flower-wall-img");
+    if (n > images.length) {
+        imgIndex = 1;
+    };
+    if (n < 1) {
+        imgIndex = images.length;
+    }
+    for (i = 0; i < images.length; i++) {
+        images[i].style.display = "none";
+    }
+    images[imgIndex-1].style.display = "block";
+}
+
